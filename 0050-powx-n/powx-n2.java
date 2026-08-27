34cefa44218ec48aa37dd5712e9ac7739125a1d9
Talha Shaikh <italhask9890@gmail.com>
2026-08-28 00:09:04 +0530

    Update powx-n2.java
    Refactor myPow method to use recursion and simplify logic.

--- 0050-powx-n/powx-n2.java
+++ 0050-powx-n/powx-n2.java
@@ -1,22 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
         long N = n;
-        
+
         if (N < 0) {
             x = 1 / x;
             N = -N;
         }
 
-        double ans = 1.0;
+        double half = myPow(x, (int)(N / 2));
 
-        while (N > 0) {
-            if ((N & 1) == 1) {
-                ans *= x;
-            }
-            x *= x;
-            N >>= 1;
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
         }
-
-        return ans;
     }
 }
